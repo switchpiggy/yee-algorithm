b365835752c44eaa55bf89343e2b799e7e87b531
switchpiggy <alanxiao211@gmail.com>
2024-05-07 12:48:51 -0700

    fixed bugs

--- yeeserver/settings.py
+++ yeeserver/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+DISABLE_COLLECTSTATIC = True
 STATIC_URL = os.path.join(BASE_DIR, 'static/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
