e510cc9b2570973923f69fc7bf8c35f944d6b9ea
switchpiggy <alanxiao211@gmail.com>
2024-05-09 18:38:54 -0700

    allowed CORS all origins

--- yeeserver/settings.py
+++ yeeserver/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-CORS_ORIGIN_ALLOW_ALL = DEBUG
+CORS_ORIGIN_ALLOW_ALL = True
 
 ALLOWED_HOSTS = ['*']
 
